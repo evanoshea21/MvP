1d78f4e88ad31a1575c1b60ca2bb9879d067462c
Evan OShea <evanoshea21@gmail.com>
2023-01-10 23:29:23 -0800

    Expenses layout great

--- CLIENT/src/components/ExpenseItem.jsx
+++ CLIENT/src/components/ExpenseItem.jsx
@@ -3,8 +3,27 @@ import React from 'react'
 const ExpenseItem = (props) => {
   return (
     <div className='expense-item'>
-
-
+      <div>
+        {/* <span>TITLE</span> */}
+        <span>Netflix</span>
+      </div>
+      <div>
+        {/* <span>CATEGORY</span> */}
+        <span>Subscription</span>
+      </div>
+      <div>
+        {/* <span>TYPE</span> */}
+        <span>Discretionary</span>
+      </div>
+      <div>
+        {/* <span>DUE DATE</span> */}
+        <span>18th</span>
+      </div>
+      <div>
+        {/* <span>AMOUNT</span> */}
+        <span>$ 12</span>
+      </div>
+        <button>Delete</button>
     </div>
   )
 }
--- CLIENT/src/components/Expenses.jsx
+++ CLIENT/src/components/Expenses.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import ExpenseItem from './ExpenseItem.jsx'
 
 const Expenses = (props) => {
   return (
     <div className='expenses'>
       <h1>Expenses</h1>
-
+      <div className='expense-title'>
+          <div>TITLE</div>
+          <div>CATEGORY</div>
+          <div>TYPE</div>
+          <div>DUE DATE</div>
+          <div>AMOUNT</div>
+      </div>
+      <ExpenseItem />
+      <ExpenseItem />
+      <ExpenseItem />
     </div>
   )
 }
