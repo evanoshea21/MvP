b9cb5a290ed7cb5771d54686a5620ab703243da8
Evan OShea <evanoshea21@gmail.com>
2023-01-11 12:18:30 -0800

    buttons and headers added

--- CLIENT/src/components/FinanceSidebar.jsx
+++ CLIENT/src/components/FinanceSidebar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const FinanceSidebar = () => {
   return (
-    <div className='finance-sidebar'>FinanceSidebar</div>
+    <div className='finance-sidebar'>
+      <div className='finance-tips'>
+        <h3>Financial Tip</h3>
+        "Clean your room, save some money..."
+      </div>
+      <div className='finance-utils'>
+        <h2>Utilities</h2>
+        <button>Add Finance Goal</button>
+      </div>
+    </div>
   )
 }
 
